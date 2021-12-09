3a6d842b63ca595243d977d038b3fb9ebd1ed253
dariokolak <kolak.dario@hotmail.com>
2021-12-09 13:33:17 +0100

    Implent responsive design and the mobile navigation for the website
    This commit will modify index.html, ./resources/css/general.css and ./resources/css/style.css files in order to implement reponsive design. This commit will also modify ./resources/js/script.js file in order to implement the mobile navigation. Also, this commit will update the height of the images in the gallery.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,15 @@
 'use strict';
 
+const headerEl = document.querySelector('.header');
+const navbarBtnEl = document.querySelector('.navbar__btn');
+
+const toggleNavState = function () {
+  navbarBtnEl.addEventListener('click', function () {
+    headerEl.classList.toggle('nav-open');
+  });
+};
+
+toggleNavState();
+
 // GLightbox
 const lightbox = GLightbox();
