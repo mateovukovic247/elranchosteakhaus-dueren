ea5dfa0341ff798051ede8f1925be3e2d4d06ee2
mateovukovic247 <mateo.vukovic247@gmail.com>
2021-12-10 15:09:30 +0100

    Fix flexbox gap
    This commit will modify ./resources/css/style.css and ./resources/js/script.js files in order to fix flexbox gap property issue for some browsers

--- resources/js/script.js
+++ resources/js/script.js
@@ -99,5 +99,25 @@ const handleFormSubmit = function () {
 
 handleFormSubmit();
 
+// Fix Flexbox gap if needed
+
+const fixFlexboxGap = function () {
+  const flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
+
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
+
+  document.body.appendChild(flex);
+  const isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
+};
+
+fixFlexboxGap();
+
 // GLightbox
 const lightbox = GLightbox();
