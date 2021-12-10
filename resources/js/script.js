9c09c81dbf6a73ceefdbe1c13df17792b2ecfc01
mateovukovic247 <mateo.vukovic247@gmail.com>
2021-12-10 13:29:13 +0100

    Implement smooth scrolling and add Impressum for the website
    This commit will modify index.html file and ./resources/js/script.js file in order to implement smooth scrolling on the website. It will use an external pollyfill script so that all major browsers support smooth scrolling. This commit will also add Impressum to the website.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const headerEl = document.querySelector('.header');
-const navbarBtnEl = document.querySelector('.navbar__btn');
 const navbarEl = document.querySelector('.navbar');
+const navbarBtnEl = document.querySelector('.navbar__btn');
+const linksEl = document.querySelectorAll('a:link');
 
 // Toggle navigation state
 const toggleNavState = function () {
@@ -36,6 +37,31 @@ const toggleNavbar = function () {
 };
 
 toggleNavbar();
+
+// Link management
+
+const scrollLinks = function () {
+  linksEl.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const href = link.getAttribute('href');
+      const dataGallery = link.getAttribute('data-gallery');
+      if (href === '#') {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      } else if (href !== '#' && href.startsWith('#')) {
+        const sectionEl = document.querySelector(href);
+        sectionEl.scrollIntoView({ behavior: 'smooth' });
+      } else if (!dataGallery) {
+        window.open(href, '_blank');
+      }
+
+      if (link.classList.contains('navbar__link'))
+        headerEl.classList.toggle('nav-open');
+    });
+  });
+};
+
+scrollLinks();
 
 // GLightbox
 const lightbox = GLightbox();
