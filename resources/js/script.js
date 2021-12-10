0aa0aa49349de3116cb4398b1f4f744df2939b2c
dariokolak <kolak.dario@hotmail.com>
2021-12-10 10:53:14 +0100

    Implement sticky navbar for the website
    This commit will modify ./resources/css/style.css and ./resources/js/script.js files in order to implement the sticky navbar for the website with the help of the Intersection Observer API.

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,7 +2,9 @@
 
 const headerEl = document.querySelector('.header');
 const navbarBtnEl = document.querySelector('.navbar__btn');
+const navbarEl = document.querySelector('.navbar');
 
+// Toggle navigation state
 const toggleNavState = function () {
   navbarBtnEl.addEventListener('click', function () {
     headerEl.classList.toggle('nav-open');
@@ -10,6 +12,30 @@ const toggleNavState = function () {
 };
 
 toggleNavState();
+
+// Toggle navbar
+
+const toggleNavbar = function () {
+  const observer = new IntersectionObserver(
+    function (entries) {
+      const ent = entries[0];
+      if (!ent.isIntersecting) {
+        navbarEl.classList.add('sticky');
+      } else {
+        navbarEl.classList.remove('sticky');
+      }
+    },
+    {
+      root: null,
+      rootMargin: '-10%',
+      threshold: 0,
+    }
+  );
+
+  observer.observe(headerEl);
+};
+
+toggleNavbar();
 
 // GLightbox
 const lightbox = GLightbox();
